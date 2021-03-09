c31e1c124b0a36bbead35fe4b6c42b1448fde751
Jared Bosma <jared.bosma@kellogg.northwestern.edu>
2021-03-08 22:54:56 -0600

    commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let db = firebase.firestore()
+
 firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
     // Signed in
@@ -13,7 +15,53 @@ firebase.auth().onAuthStateChanged(async function(user) {
       document.location.href = 'index.html'
     })
   
-  
+    document.querySelector('.sign-out').addEventListener('click', function(event) {
+      console.log('sign out clicked')
+      firebase.auth().signOut()
+      document.location.href = 'index.html'
+    })
+
+    // Listen for the form submit and create/render the new post
+    document.querySelector('form').addEventListener('submit', async function(event) {
+      event.preventDefault()
+      let postUsername = user.displayName
+      let postImageUrl = document.querySelector('#image-url').value
+      let postDish = document.querySelector('#dish').value
+      let postRestaurant = document.querySelector('#restaurant').value
+      let postPrice = document.querySelector('#price').value
+      let postRating = document.querySelector('#rating').value
+      
+      //will need to convert into netlify that is commented out below
+      let addPost = await db.collection('dishes').add({
+        username: postUsername,
+        imageUrl: postImageUrl,
+        dish: postDish,
+        restaurant: postRestaurant,
+        price: postPrice,
+        rating: postRating,
+        likes: 0
+      })
+
+      // let response = await fetch('/.netlify/functions/create_post', {
+      //   method: 'POST',
+      //   body: JSON.stringify({
+      //     userId: user.uid,
+      //     username: postUsername,
+      //     imageUrl: postImageUrl
+      //   })
+      // })
+      
+      // let post = await response.json()
+      // document.querySelector('#image-url').value = '' // clear the image url field
+      // renderPost(post)
+    })
+
+    // let response = await fetch('/.netlify/functions/get_posts')
+    // let posts = await response.json()
+    // for (let i=0; i<posts.length; i++) {
+    //   let post = posts[i]
+    //   renderPost(post)
+    // }
   
   
   } else {
