c62e805bdbd04b074663898bedf1ab69501ff477
Jared Bosma <jared.bosma@kellogg.northwestern.edu>
2021-03-09 10:38:57 -0600

    commit

--- index.js
+++ index.js
@@ -30,86 +30,95 @@ firebase.auth().onAuthStateChanged(async function(user) {
       let postRestaurant = document.querySelector('#restaurant').value
       let postPrice = document.querySelector('#price').value
       let postRating = document.querySelector('#rating').value
-      
-      //will need to convert into netlify function that is commented out below
-      let addPost = await db.collection('dishes').add({
-        username: postUsername,
-        imageUrl: postImageUrl,
-        dish: postDish,
-        restaurant: postRestaurant,
-        price: postPrice,
-        rating: postRating,
-        likes: 0
-      })
-    })
 
-      // let response = await fetch('/.netlify/functions/create_post', {
-      //   method: 'POST',
-      //   body: JSON.stringify({
-      //     userId: user.uid,
-      //     username: postUsername,
-      //     imageUrl: postImageUrl
-      //   })
+      //will need to convert into netlify function that is commented out below
+      // let addPost = await db.collection('dishes').add({
+      //   username: postUsername,
+      //   imageUrl: postImageUrl,
+        // dish: postDish,
+        // restaurant: postRestaurant,
+        // price: postPrice,
+        // rating: postRating,
+        // likes: 0
       // })
       
+      //now converted to netlify 
+      let response = await fetch('/.netlify/functions/create_dish', {
+        method: 'POST',
+        body: JSON.stringify({
+          userId: user.uid,
+          username: postUsername,
+          imageUrl: postImageUrl,
+          dish: postDish,
+          restaurant: postRestaurant,
+          price: postPrice,
+          rating: postRating,
+          likes: 0
+        })
+      })
+      let dish = await response.json()
+      document.querySelector('#image-url').value = "" // clear image URL field
+      document.querySelector('#dish').value = ""
+      document.querySelector('#restaurant').value = ""
+      document.querySelector('#price').value = ""
+      document.querySelector('#rating').value = ""
+      renderPost(dish)
+      // console.log(dish)
+    }) 
       //will need to replace with json netfliy functions later 
-        let querySnapshot = await db.collection('dishes').get()
-        let dishes = querySnapshot.docs
+        // let querySnapshot = await db.collection('dishes').get()
+        // let dishes = querySnapshot.docs
 
-      for(let i = 0; i<dishes.length; i++) {
-        let dishId = dishes[i].id 
-        let dishData = dishes[i].data()
-        let dishUsername = dishData.username
-        let dishImageUrl = dishData.imageUrl
-        let dishDish = dishData.dish
-        let dishRestaurant = dishData.restaurant
-        let dishPrice = dishData.price
-        let dishRating = dishData.rating
-        let dishLikes = dishData.likes
-
-        console.log(dishDish)
-
-        document.querySelector('.dishes').insertAdjacentHTML('beforeend', `
-          <div class="dish-${dishId} md:mt-16 mt-8 space-y-8">
-            <div class="md:mx-0 mx-4">
-              <span class="font-bold text-xl">${dishDish}</span>
-            </div>
-            <div>
-              <img src="${dishImageUrl}" class="w-full">
-            </div>
-      
-            <div class="text-3xl md:mx-0 mx-4">
-              <button class="like-button">❤️</button>
-              <span class="likes">${dishLikes}</span>
-              <span class="rating">${dishRating}/10 </span>
-              <span class="price"> $${dishPrice}</span>
-              <span class="username text-sm text-right font-light">@${dishUsername}</span>
-            </div>
-          </div>
-        `)
-        document.querySelector(`.dish-${dishId} .like-button`).addEventListener('click', async function(event){  
-          event.preventDefault()
-          console.log(`dish ${dishId} like button clicked!`)
-          let existingNumberOfLikes = document.querySelector(`.dish-${dishId} .likes`).innerHTML
-          let newNumberOfLikes= parseInt(existingNumberOfLikes) + 1
-          document.querySelector(`.dish-${dishId} .likes`).innerHTML = newNumberOfLikes
-          await db.collection('dishes').doc(dishId).update({
-            likes: firebase.firestore.FieldValue.increment(1)
-          })
-        })      
-      }
-      // let post = await response.json()
-      // document.querySelector('#image-url').value = '' // clear the image url field
-      // renderPost(post)
+      // for(let i = 0; i<dishes.length; i++) {
+      //   let dishId = dishes[i].id 
+      //   let dishData = dishes[i].data()
+      //   let dishUsername = dishData.username
+      //   let dishImageUrl = dishData.imageUrl
+      //   let dishDish = dishData.dish
+      //   let dishRestaurant = dishData.restaurant
+      //   let dishPrice = dishData.price
+      //   let dishRating = dishData.rating
+      //   let dishLikes = dishData.likes
+      //   console.log(dishDish)
+   
+    let response = await fetch('/.netlify/functions/get_dishes')
+    let dishes = await response.json()
+    for (let i=0; i<dishes.length; i++) {
+      let dish = dishes[i]
+      renderPost(dish)
     
+      
+        // document.querySelector('.dishes').insertAdjacentHTML('beforeend', `
+        //   <div class="dish-${dishId} md:mt-16 mt-8 space-y-8">
+        //     <div class="md:mx-0 mx-4">
+        //       <span class="font-bold text-xl">${dishDish}</span>
+        //     </div>
+        //     <div>
+        //       <img src="${dishImageUrl}" class="w-full">
+        //     </div>
+      
+        //     <div class="text-3xl md:mx-0 mx-4">
+        //       <button class="like-button">❤️</button>
+        //       <span class="likes">${dishLikes}</span>
+        //       <span class="rating">${dishRating}/10 </span>
+        //       <span class="price"> $${dishPrice}</span>
+        //       <span class="username text-sm text-right font-light">@${dishUsername}</span>
+        //     </div>
+        //   </div>
+        // `)
 
-    // let response = await fetch('/.netlify/functions/get_posts')
-    // let posts = await response.json()
-    // for (let i=0; i<posts.length; i++) {
-    //   let post = posts[i]
-    //   renderPost(post)
-    // }
-  
+        // //add likes when the button is clicked 
+        // document.querySelector(`.dish-${dishId} .like-button`).addEventListener('click', async function(event){  
+        //   event.preventDefault()
+        //   console.log(`dish ${dishId} like button clicked!`)
+        //   let existingNumberOfLikes = document.querySelector(`.dish-${dishId} .likes`).innerHTML
+        //   let newNumberOfLikes= parseInt(existingNumberOfLikes) + 1
+        //   document.querySelector(`.dish-${dishId} .likes`).innerHTML = newNumberOfLikes
+        //   await db.collection('dishes').doc(dishId).update({
+        //     likes: firebase.firestore.FieldValue.increment(1)
+        //   })
+        // })        
+    }
   
   } else {
     // Signed out
@@ -130,3 +139,54 @@ firebase.auth().onAuthStateChanged(async function(user) {
     ui.start('.sign-in-or-sign-out', authUIConfig)
   }
 })
+
+async function renderPost(dish) {
+  let dishId = dish.id
+  document.querySelector('.dishes').insertAdjacentHTML('beforeend', `
+    <div class="dish-${dishId} md:mt-16 mt-8 space-y-8">
+      <div class="md:mx-0 mx-4">
+        <span class="font-bold text-xl">${dish.dish}</span>
+      </div>
+      <div>
+        <img src="${dish.imageUrl}" class="w-full">
+      </div>
+
+      <div class="text-3xl md:mx-0 mx-4">
+        <button class="like-button">❤️</button>
+        <span class="likes">${dish.likes}</span>
+        <span class="rating">${dish.rating}/10 </span>
+        <span class="price"> $${dish.price}</span>
+        <span class="username text-sm text-right font-light">@${dish.username}</span>
+      </div>
+    </div>
+  `)
+
+  //add likes when the button is clicked 
+  // document.querySelector(`.dish-${dishId} .like-button`).addEventListener('click', async function(event){
+  let likeButton = document.querySelector(`.dish-${dishId} .like-button`)
+  likeButton.addEventListener('click', async function(event) {  
+    event.preventDefault()
+    console.log(`dish ${dishId} like button clicked!`)
+    let currentUserId = firebase.auth().currentUser.uid
+
+    let response = await fetch('/.netlify/functions/like', {
+      method: 'POST',
+      body: JSON.stringify({
+        dishId: dishId,
+        userId: currentUserId
+      })
+    })
+    if (response.ok) {
+      let existingNumberOfLikes = document.querySelector(`.dish-${dishId} .likes`).innerHTML
+      let newNumberOfLikes = parseInt(existingNumberOfLikes) + 1
+      document.querySelector(`.dish-${dishId} .likes`).innerHTML = newNumberOfLikes
+    }
+
+    // let existingNumberOfLikes = document.querySelector(`.dish-${dishId} .likes`).innerHTML
+    // let newNumberOfLikes= parseInt(existingNumberOfLikes) + 1
+    // document.querySelector(`.dish-${dishId} .likes`).innerHTML = newNumberOfLikes
+    // await db.collection('dishes').doc(dishId).update({
+    //   likes: firebase.firestore.FieldValue.increment(1)
+    // })
+  }) 
+}
